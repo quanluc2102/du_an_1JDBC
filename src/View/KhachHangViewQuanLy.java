3c33bde5700c72a88bc6e99d71f16dd8e03ab55e
quanluc2102 <quanluc2102@gmail.com>
2022-12-07 11:00:51 +0700

    sửa nốt

--- src/View/KhachHangViewQuanLy.java
+++ src/View/KhachHangViewQuanLy.java
@@ -869,9 +869,7 @@ public class KhachHangViewQuanLy extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        TrangChuQuanLyView detail1 = new TrangChuQuanLyView("");
         this.dispose();
-        
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
