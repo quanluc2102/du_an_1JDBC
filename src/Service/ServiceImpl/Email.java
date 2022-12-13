69960bfe54f4a42c64273d6f503dfc25ed3036f1
DESKTOP-SS916DG\haha <vietttph20936@fpt.edu.vn>
2022-12-14 02:23:45 +0700

    Email bug

--- src/Service/ServiceImpl/Email.java
+++ src/Service/ServiceImpl/Email.java
@@ -5,12 +5,15 @@
 package Service.ServiceImpl;
 
 import java.io.File;
+import java.util.Date;
 import java.util.Properties;
 import javax.activation.DataSource;
 import javax.activation.FileDataSource;
+import javax.mail.Authenticator;
 import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -72,35 +75,34 @@ public class Email implements Service.Email {
 
     @Override
     public String sendEmail(String fromEmail, String passWord, String toEmail, String subject, String body) {
-        try {
 
+        try {
             Properties props = new Properties();
             props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
             props.put("mail.smtp.port", "587"); //TLS Port
             props.put("mail.smtp.auth", "true"); //enable authentication
             props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
-
-            Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
-                protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-                    return new javax.mail.PasswordAuthentication(fromEmail, passWord);
+            Authenticator auth = new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(fromEmail, passWord);
                 }
-            });
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(fromEmail));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
-            message.setSubject(subject);
-            // Phan 1 gom doan tin nhan
-            BodyPart messageBodyPart1 = new MimeBodyPart();
-            messageBodyPart1.setText(body);
-
-            // phan 2 chua tap tin image
-            // Duong dan den file cua ban
-            Transport.send(message);
+            };
+            Session session = Session.getInstance(props, auth);
+            MimeMessage msg = new MimeMessage(session);
+            //set message headers
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+            msg.addHeader("format", "flowed");
+            msg.addHeader("Content-Transfer-Encoding", "8bit");
+            msg.setFrom(new InternetAddress(fromEmail, "NoReply-JD"));
+            msg.setReplyTo(InternetAddress.parse(fromEmail, false));
+            msg.setSubject(subject, "UTF-8");
+            msg.setText(body, "UTF-8");
+            msg.setSentDate(new Date());
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+            Transport.send(msg);
             System.out.println("Gui mail thanh cong");
-
-            return "Gửi thành công";
+            return "Gửi thất bại";
         } catch (Exception e) {
-            e.printStackTrace();
             return "Gửi thất bại";
         }
     }
